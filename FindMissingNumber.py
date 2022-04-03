b5204b2506b100a75cfb26ef051342e8dea07410
Mark Karels <mak8966@uncw.edu>
2022-04-03 17:14:39 -0400

    Solved Problem

--- FindMissingNumber.py
+++ FindMissingNumber.py
@@ -7,3 +7,18 @@ in a list and k, and returns the missing numbers in ascending order (from smalle
 For example, missing_nos([1, 2, 4, 5, 6, 7, 8, 10], 2) should return [3, 9].
 """
 
+
+def missing_nos(arr, k):
+    a = arr.copy()
+    b = []
+    for i in range(len(a) - 1):
+        if ((a[i + 1]) - a[i]) != 1:
+            b.append(a[i] + 1)
+    if len(b) != k:
+        return print('Length of list not matching number of missing tiles!')
+
+    b.sort()
+    return b
+
+
+print(missing_nos([1, 2, 4, 5, 6, 7, 8, 10], 2))

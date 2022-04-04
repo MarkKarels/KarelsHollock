d9c0c2ea379ddc3b4b3b832df789352990f7d756
Mark Karels <mak8966@uncw.edu>
2022-04-04 08:46:00 -0400

    Solved Challange

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -7,8 +7,25 @@ For example, fizzbuzz(16) should return
 [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz', 16].
 """
 
+
 def fizzbuzz(limit):
-  # Write your code here
+    lst = []
+    count = 1
+    for i in range(limit):
+        if count % 3 != 0 and count % 5 != 0:
+            lst.append(count)
+            count += 1
+        elif count % 3 == 0 and count % 5 != 0:
+            lst.append("Fizz")
+            count += 1
+        elif count % 3 != 0 and count % 5 == 0:
+            lst.append("Buzz")
+            count += 1
+        else:
+            lst.append("FizzBuzz")
+            count += 1
+
+    return lst
 
 
 print(fizzbuzz(16))
